23d987b1a73908ae876f2e01d6a346ac3579c31f
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-04 23:56:42 +0100

    outline team

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+	import saurav from "$lib/images/team/saurav.jpg";
+	import carl from "$lib/images/team/carl.jpg";
+	import jignesh from "$lib/images/team/jignesh.jpg";
 </script>
 
 <svelte:head>
@@ -8,18 +11,49 @@
 
 <section>
 	<div class="container">
+
 		<div class="team-section">
-			<div class="team-card">Saurav</div>
-			<div class="team-card">Carl</div>
-			<div class="team-card">Jignesh</div>
+
+			<div class="team-card">
+				<img src={saurav} alt="saurav" />
+				<div class="team-name">Saurav Kumar</div>
+				<div class="team-title">President</div>
+				<div class="team-info">
+					<div class="button">linkedin</div>
+					<div class="button">twitter</div>
+				</div>
+			</div>
+
+			<div class="team-card">
+				<img src={carl} alt="carl" />
+				<div class="team-name">Carl Ulvinen</div>
+				<div class="team-title">Vice President</div>
+				<div class="team-info">
+					<div class="button">linkedin</div>
+					<div class="button">twitter</div>
+				</div>
+			</div>
+
+			<div class="team-card">
+				<img src={jignesh} alt="saurav" />
+				<div class="team-name">Jignesh Beladiya</div>
+				<div class="team-title">Writer</div>
+				<div class="team-info">
+					<div class="button">linkedin</div>
+					<div class="button">twitter</div>
+				</div>
+			</div>
+
 		</div>
+
 		<div class="team-section">
+
 			<div class="team-card">Briws</div>
 			<div class="team-card">Abhimanyu</div>
 			<div class="team-card">Jwalin</div>
 			<div class="team-card">Abishek</div>
-		</div>
 
+		</div>
 
 	</div>
 </section>
@@ -61,9 +95,42 @@
 	}
 
 	.team-card{
+
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
+		align-items: center;
 		margin: 20px;
+	}
+
+	.team-card img{
+		width: 128px;
+		border-radius: var(--border-radius);
+	}
+	
+	.team-name{
+
+	}
+
+	.team-title{
+
+	}
+
+	.team-info{
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+
+	.button{
+		background-color: var(--button-color);
+		font-size: 12px;
+		padding: 10px;
+		margin: 5px;
+		border-radius: var(--border-radius);
+	}
+
+	.button:hover{
+		background-color: var(--button-color-alt);
 	}
 </style>
