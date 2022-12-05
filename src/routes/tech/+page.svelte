e2167801189909abb81fe2019a2b60558d1c3dfa
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-05 02:48:44 +0100

    revision

--- src/routes/tech/+page.svelte
+++ src/routes/tech/+page.svelte
@@ -18,6 +18,28 @@
 </svelte:head>
 
 <section>
+
+	<div class="multiple-containers">
+		<div class="one-quarter">
+			<div class="title">Meet Hydro</div>
+			<div class="information">
+				The Hydro protocols are available to use on multiple networks. 
+                All of our protocols and dApps are powered by Hydro tokens.
+				<br><br>
+				Hydro has a total supply of 111,111,111 which was elected by its community. 
+                The distribution between chains is handled by our bridge that swaps your tokens between chains. 
+                Once the swap from one chain to another is complete you are free to enjoy the benefits of your chosen network.
+				<br><br>
+				All of the Hydro tokens that are consumed by a Smart Contract will evaporate, 
+                hence the supply will continously decrease over time.
+			</div>
+		</div>
+
+		<div class="three-quarter">
+			<img src={ecosystem} alt="hydro-ecosystem" />
+		</div>
+		</div>
+
 	<div class="multiple-containers">
 		<div class="half">
 			<div class="chains">
@@ -42,26 +64,7 @@
 		</div>
 	</div>
 
-    <div class="multiple-containers">
-		<div class="one-quarter">
-			<div class="title">Meet Hydro</div>
-			<div class="information">
-				The Hydro protocols are available to use on multiple networks. 
-                All of our protocols and dApps are powered by Hydro tokens.
-				<br><br>
-				Hydro has a total supply of 111,111,111 which was elected by its community. 
-                The distribution between chains is handled by our bridge that swaps your tokens between chains. 
-                Once the swap from one chain to another is complete you are free to enjoy the benefits of your chosen network.
-				<br><br>
-				All of the Hydro tokens that are consumed by a Smart Contract will evaporate, 
-                hence the supply will continously decrease over time.
-			</div>
-		</div>
-
-		<div class="three-quarter">
-			<img src={ecosystem} alt="hydro-ecosystem" />
-		</div>
-		</div>
+   
 </section>
 
 <style>
