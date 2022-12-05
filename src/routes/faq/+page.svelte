9f745de60cdc5a39a4700cc3484bdeb98abc0b2c
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-06 00:40:42 +0100

    outline faq

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -7,9 +7,59 @@
 </svelte:head>
 
 <section>
-	<div class="container">FAQ</div>
+	<div class="multiple-containers">
+		<div class="one-quarter">
+
+			<a href="/faq">What is a multichain network and its advantages?</a>
+
+			<a href="/faq">When did Hydro upgrade to be a multichain token? and how will it benefit the Hydro ecosystem?</a>
+
+			<a href="/faq">What is the main purpose of project Hydro?</a>
+
+			<a href="/faq">What is Hydroswap?</a>
+
+			<a href="/faq">What is Aegir wallet?</a>
+
+			<a href="/faq">What is Keresverse?</a>
+
+			<a href="/faq">How can Hydro benefit its users? And why should I choose Hydro?</a>
+
+			<a href="/faq">Did Hydro have any ICO, or received any investment?</a>
+
+			<a href="/faq">How many products of Hydro have been ready to use?</a>
+
+			<a href="/faq">How can I swap Hydro between chains to get the lower fee?</a>
+
+			<a href="/faq">Where can I stake Hydro and how much is the APR ?</a>
+
+			<a href="/faq">How can I show a Hydro BEP20 token on my wallets?</a>
+
+			<a href="/faq">How many percent of total supply does the Team hold?</a>
+
+			<a href="/faq">How many partners of Hydro till now? and which field do they engage in?</a>
+
+			<a href="/faq">What are the new Smart Contract addresses of Hydro token?</a>
+
+		</div>
+		<div class="three-quarter">
+			<div class="faq-answer">
+
+				
+			</div>
+		</div>
+	</div>
 </section>
 
 <style>
+
+	a{
+		padding: 1rem;
+		color: #fff;
+	}
+
+	.one-quarter, .three-quarter{
+		height: 60rem;
+		align-items: flex-start;
+	}
 	
 </style>
