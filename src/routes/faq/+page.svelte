e2286189647b7260b2694b659596493729f722e6
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-06 19:10:17 +0100

    refactoring

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -15,27 +15,27 @@
 
 		<div class="questions">
 
-			<a href="/q1">What is the main purpose of project Hydro?</a>
+			<a href="/questions/q1">What is the main purpose of project Hydro?</a>
 
-			<a href="/q2">What is Hydroswap?</a>
+			<a href="/questions/q2">What is Hydroswap?</a>
 
-			<a href="/q3">What is Aegir wallet?</a>
+			<a href="/questions/q3">What is Aegir wallet?</a>
 
-			<a href="/q4">What is Keresverse?</a>
+			<a href="/questions/q4">What is Keresverse?</a>
 
-			<a href="/q5">How can Hydro benefit its users?</a>
+			<a href="/questions/q5">How can Hydro benefit its users?</a>
 
-			<a href="/q6">How to swap Hydro between blockchains?</a>
+			<a href="/questions/q6">How to swap Hydro between blockchains?</a>
 
-			<a href="/q7">How to stake Hydro and what are the rewards?</a>
+			<a href="/questions/q7">How to stake Hydro and what are the rewards?</a>
 
-			<a href="/q8">How to add Hydro Token in a Web3 wallet?</a>
+			<a href="/questions/q8">How to add Hydro Token in a Web3 wallet?</a>
 
-			<a href="/q9">Smart Contract addresses of the Hydro token?</a>
+			<a href="/questions/q9">Smart Contract addresses of the Hydro token?</a>
 
-			<a href="/q10">Did Hydro have any ICO, or received any investment?</a>
+			<a href="/questions/q10">Did Hydro have any ICO, or received any investment?</a>
 
-			<a href="/q11">How many percent of total supply does the Team hold?</a>
+			<a href="/questions/q11">How many percent of total supply does the Team hold?</a>
 
 		</div>
 		
