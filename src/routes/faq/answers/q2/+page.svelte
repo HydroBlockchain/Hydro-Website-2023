c4c559daa41f354bdd6929676679cbb912abf2bf
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-08 13:38:48 +0100

    faq second

--- src/routes/faq/answers/q2/+page.svelte
+++ src/routes/faq/answers/q2/+page.svelte
@@ -8,11 +8,20 @@
 
 <section>
 	<div class="container">
-        What is Hydroswap?
+        <div class="faq-title">What is Hydroswap?</div>
         <br><br>
-        Answer, bla bla
-        <br> <br>
-        <a href="/faq">Return</a>
+        <div class="faq-answer">
+            HydroSwap is not your typical decentralized exchange that the DeFi space comes across regularly. 
+            It has evolved to involve much more than that. It runs on the Binance Smart Chain to offer high scalability and better transactional experience by users. 
+            With this platform, users can seamlessly exchange their tokens/coins for other tokens/coins. 
+            They can also become liquidity providers for lucrative rewards. 
+            The use cases of HydroSwap continue to increase as newer versions are released into the market.
+        <div class="faq-list">To ensure that everyone, not minding their level of knowledge in the blockchain technology can enjoy the features that HydroSwap offers, 
+            the user interface was designed to improve user experience. It is easy to swap tokens without paying a hand and a leg to do that. 
+            With HydroSwap, users can easily trade their tokens, stake them and earn. For those that want to create liquidity pools, it is feasible.
+        </div>
+        </div>
+        <div class="button"><a href="/faq">Return</a></div>
     </div>
 </section>
 
