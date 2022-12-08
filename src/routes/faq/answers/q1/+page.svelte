c416a35acc4d56d389a2fd235d0bae62666b1951
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-08 13:25:06 +0100

    first faq answer

--- src/routes/faq/answers/q1/+page.svelte
+++ src/routes/faq/answers/q1/+page.svelte
@@ -8,11 +8,24 @@
 
 <section>
 	<div class="container">
-        What is the main purpose of project Hydro?
+        <div class="faq-title">What is the main purpose of project Hydro?</div>
         <br><br>
-        Answer, bla bla
-        <br> <br>
-        <a href="/faq">Return</a>
+        <div class="faq-answer">
+        Project Hydro is web3, multi-chain decentralized ecosystem which uses cutting-edge cryptographic technology to secure user identities, accounts, and transactions. 
+        Hydro aims to decentralize financial services by bringing public blockchain technology to traditional private systems. 
+        It also allows developers around the world to boost their platforms and applications with this innovative technology.
+        As web 3.0 is our coming future and semantic web has already been used, where data is arranged and kept in such a way that computers can easily decipher at a faster pace.
+        <div class="faq-list">       
+        Project Hydro has multiple projects deployed on multiple blockchain’s under our umbrella such as <br><br>
+        <div class="faq-lis-entry">1. Aegir (Ægir) Wallet (multi-coin/token wallet)</div>
+        <div class="faq-lis-entry">2. Hydro Swap (Decentralize exchange with Automated Market Making)</div>
+        <div class="faq-lis-entry">3. Keresverse (MetaVerse First Person Shooter Game)</div>
+        <div class="faq-lis-entry">4. Snowflake (Tokenized KYC)</div>
+        <div class="faq-lis-entry">5. Raindrop (Multi Factor Authentication Leveraging blockchain)</div>
+        To name a few.
+        </div>
+        </div>
+        <div class="button"><a href="/faq">Return</a></div>
     </div>
 </section>
 
