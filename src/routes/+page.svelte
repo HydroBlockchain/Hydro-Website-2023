338fe34e26eb6187b67ca0cf041cd87fe83b4520
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-08 15:42:51 +0100

    three latest articles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,12 +30,20 @@
 	 //Get the latest Article
 	 let blogTitle = [];
 	 let blogLink = [];
+	 let blogTitleSecond = [];
+	 let blogLinkSecond = [];
+	 let blogTitleThird = [];
+	 let blogLinkThird = [];
 	 async function getData() {
 	 	const response = await fetch(apiURL);
 	 	if (response) {
 	 		data = await response.json();
 	 		blogTitle = data.items[0].title;
 	 		blogLink = data.items[0].link;
+			blogTitleSecond = data.items[1].title;
+	 		blogLinkSecond = data.items[1].link;
+			blogTitleThird = data.items[2].title;
+	 		blogLinkThird = data.items[2].link;
 			// console.log(blogTitle);
 			// console.log(blogLink);
 	 	} else {
@@ -59,8 +67,12 @@
 	<div class="multiple-containers">
 			<div class="one-quarter" id="intro-bg">Introduction</div>
 			<div class="three-quarter" id="blog-bg">
+				<div class="blog-posts">
 				<div class="latest">Lastest Hydro Blog Post</div>
 				<div class="blog-title"><a href ="{(blogLink)}" target="_blank" rel="noopener noreferrer"> {(blogTitle)} </a></div>
+				<div class="blog-title"><a href ="{(blogLinkSecond)}" target="_blank" rel="noopener noreferrer"> {(blogTitleSecond)} </a></div>
+				<div class="blog-title"><a href ="{(blogLinkThird)}" target="_blank" rel="noopener noreferrer"> {(blogTitleThird)} </a></div>
+			</div>
 		</div>
 	</div>
 
