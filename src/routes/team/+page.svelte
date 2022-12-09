4bdc31a7587bf223f7323583ab5d5759b97f0b3e
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-09 20:34:32 +0100

    team.json as data source

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+// @ts-nocheck
 import saurav from "$lib/images/team/saurav.jpg";
 import carl from "$lib/images/team/carl.jpg";
 import jignesh from "$lib/images/team/jignesh.jpg";
@@ -6,6 +7,7 @@ import ayushya from "$lib/images/team/ayushya.jpg";
 import profile from "$lib/images/team/profile.jpg";
 import twitter from "$lib/images/icons/twitter.svg";
 import linkedin from "$lib/images/icons/linkedin.svg";
+import item from "./team.json"
 </script>
 
 <svelte:head>
@@ -20,31 +22,37 @@ import linkedin from "$lib/images/icons/linkedin.svg";
 
                 <div class="team-card">
                     <img src={saurav} alt="saurav" id="profile-pic"/>
-                    <div class="team-name">Saurav Kumar</div>
-                    <div class="team-title">President</div>
+                    <div class="team-name">{item[0].name}</div>
+                    <div class="team-title">{item[0].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[0].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[0].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
                 <div class="team-card">
                     <img src={carl} alt="carl" id="profile-pic"/>
-                    <div class="team-name">Carl Ulvinen</div>
-                    <div class="team-title">Vice President</div>
+                    <div class="team-name">{item[1].name}</div>
+                    <div class="team-title">{item[1].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[1].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[1].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
-                    <div class="team-name">Abhimanyu Bundela</div>
-                    <div class="team-title">Chief Gaming Officer</div>
+                    <div class="team-name">{item[2].name}</div>
+                    <div class="team-title">{item[2].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[2].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[2].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
@@ -54,41 +62,49 @@ import linkedin from "$lib/images/icons/linkedin.svg";
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
-                    <div class="team-name">Briws Ustyugov</div>
-                    <div class="team-title">Frontend Developer</div>
+                    <div class="team-name">{item[3].name}</div>
+                    <div class="team-title">{item[3].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[3].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[3].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
                 <div class="team-card">
                     <img src={ayushya} alt="ayushya" id="profile-pic"/>
-                    <div class="team-name">Ayushya Chitransh</div>
-                    <div class="team-title">Backend Developer</div>
+                    <div class="team-name">{item[4].name}</div>
+                    <div class="team-title">{item[4].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[4].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[4].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
-                    <div class="team-name">Jwalin Adhyaru</div>
-                    <div class="team-title">3D Artist</div>
+                    <div class="team-name">{item[5].name}</div>
+                    <div class="team-title">{item[5].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[5].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[5].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
-                    <div class="team-name">Abishek Padmanaban</div>
-                    <div class="team-title">3D Artist</div>
+                    <div class="team-name">{item[6].name}</div>
+                    <div class="team-title">{item[6].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[6].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[6].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
@@ -98,41 +114,49 @@ import linkedin from "$lib/images/icons/linkedin.svg";
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
-                    <div class="team-name">Sundar Mahalingam</div>
-                    <div class="team-title">3D Artist</div>
+                    <div class="team-name">{item[7].name}</div>
+                    <div class="team-title">{item[7].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[7].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[7].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
                 <div class="team-card">
                     <img src={jignesh} alt="jignesh" id="profile-pic"/>
-                    <div class="team-name">Jignesh Beladiya</div>
-                    <div class="team-title">Content Creator</div>
+                    <div class="team-name">{item[8].name}</div>
+                    <div class="team-title">{item[8].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[8].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[8].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
-                    <div class="team-name">Anochie Modestus</div>
-                    <div class="team-title">Community Manager</div>
+                    <div class="team-name">{item[9].name}</div>
+                    <div class="team-title">{item[9].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com/uzobankz" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://www.linkedin.com/in/anochie-modestus-26b75616a" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[9].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[9].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
-                    <div class="team-name">Matas</div>
-                    <div class="team-title">Community</div>
+                    <div class="team-name">{item[10].name}</div>
+                    <div class="team-title">{item[10].title}</div>
                     <div class="team-info">
-                        <div class="button-team"><a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" id="icon"/></a></div>
-                        <div class="button-team"><a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="linkedin" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[10].twitter}" target="_blank" rel="noopener noreferrer">
+                            <img src={twitter} alt="twitter" id="icon"/></a></div>
+                        <div class="button-team"><a href="{item[10].linkedin}" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
 
