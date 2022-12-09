70a8b37c0b4d78bda9a8591029e0cb60df87da01
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-09 04:19:06 +0100

    clean

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,61 +1,65 @@
 <script>
-	// @ts-nocheck
+// @ts-nocheck
 
-	import Header from "./Header.svelte";
-	import Footer from "./Footer.svelte";
-	import "./styles.css";
-	import { onMount } from "svelte";
-	import { state } from "$lib/stores/store.js";
-	import LoadingScreen from "$lib/components/loadingscreen.svelte";
-    import Pricebanner from "$lib/components/pricebanner.svelte";
+import Header from "./Header.svelte";
+import Footer from "./Footer.svelte";
+import "./styles.css";
+import {
+    onMount
+} from "svelte";
+import {
+    state
+} from "$lib/stores/store.js";
+import LoadingScreen from "$lib/components/loadingscreen.svelte";
+import Pricebanner from "$lib/components/pricebanner.svelte";
 
-	let ready;
+let ready;
 
-	onMount(() => {
-		ready = true;
-	});
+onMount(() => {
+    ready = true;
+});
 
-	$: {
-		if (ready) {
-			setInterval(() => {
-				state.set({ loading: false });
-			}, 1000);
-		}
-	}
+$: {
+    if (ready) {
+        setInterval(() => {
+            state.set({
+                loading: false
+            });
+        }, 1000);
+    }
+}
 </script>
 
 <!--preloader-->
 {#if $state.loading}
-	<LoadingScreen />
+<LoadingScreen />
 {/if}
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
 <div class="app-wrapper">
-	<Pricebanner />
-	<div class="app">
-		<Header />
-		<main>
-			<slot />
-		</main>
-		<Footer />
-	</div>
+    <Pricebanner />
+    <div class="app">
+        <Header />
+        <main>
+            <slot />
+        </main>
+        <Footer />
+    </div>
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+.app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+}
 
-	main {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		width: 100%;
-		max-width: 1200px;
-		margin: 0 auto;
-		margin-top: 20px;
-	}
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-top: 20px;
+}
 </style>
