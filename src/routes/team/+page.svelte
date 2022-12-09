441371e9b84b421bc720e240df587bc4c68154cd
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-09 22:11:55 +0100

    restyle team cards

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -18,10 +18,11 @@ import item from "./team.json"
     <section>
         <div class="container" id="animated-border">
 
-            <div class="team-section">
+            <div class="team-section" id="top-one">
 
                 <div class="team-card">
                     <img src={saurav} alt="saurav" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[0].name}</div>
                     <div class="team-title">{item[0].title}</div>
                     <div class="team-info">
@@ -31,9 +32,11 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
                 <div class="team-card">
                     <img src={carl} alt="carl" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[1].name}</div>
                     <div class="team-title">{item[1].title}</div>
                     <div class="team-info">
@@ -43,9 +46,11 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[2].name}</div>
                     <div class="team-title">{item[2].title}</div>
                     <div class="team-info">
@@ -55,6 +60,7 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
             </div>
 
@@ -62,6 +68,7 @@ import item from "./team.json"
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[3].name}</div>
                     <div class="team-title">{item[3].title}</div>
                     <div class="team-info">
@@ -71,9 +78,11 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
                 <div class="team-card">
                     <img src={ayushya} alt="ayushya" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[4].name}</div>
                     <div class="team-title">{item[4].title}</div>
                     <div class="team-info">
@@ -83,9 +92,11 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[5].name}</div>
                     <div class="team-title">{item[5].title}</div>
                     <div class="team-info">
@@ -95,9 +106,11 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[6].name}</div>
                     <div class="team-title">{item[6].title}</div>
                     <div class="team-info">
@@ -107,13 +120,15 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
             </div>
 
-            <div class="team-section">
+            <div class="team-section" id="last-one">
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[7].name}</div>
                     <div class="team-title">{item[7].title}</div>
                     <div class="team-info">
@@ -123,9 +138,11 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
                 <div class="team-card">
                     <img src={jignesh} alt="jignesh" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[8].name}</div>
                     <div class="team-title">{item[8].title}</div>
                     <div class="team-info">
@@ -135,9 +152,11 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[9].name}</div>
                     <div class="team-title">{item[9].title}</div>
                     <div class="team-info">
@@ -147,9 +166,11 @@ import item from "./team.json"
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
                 </div>
+                </div>
 
                 <div class="team-card">
                     <img src={profile} alt="profile" id="profile-pic"/>
+                    <div class="team-column">
                     <div class="team-name">{item[10].name}</div>
                     <div class="team-title">{item[10].title}</div>
                     <div class="team-info">
@@ -158,6 +179,7 @@ import item from "./team.json"
                         <div class="button-team"><a href="{item[10].linkedin}" target="_blank" rel="noopener noreferrer">
                             <img src={linkedin} alt="linkedin" id="icon"/></a></div>
                     </div>
+                </div>
                 </div>
 
             </div>
@@ -173,34 +195,70 @@ import item from "./team.json"
 }
 
 .team-card {
-    width: 10rem;
+    width: 270px;
+    height: 130px;
     padding: 1rem;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
-    margin: 20px;
+    margin: 0.5rem;
     animation: fadeIn 2.5s;
+    position: relative;
+	z-index: 1;
+	background: inherit;
+	overflow: hidden;
+    border-radius: var(--border-radius);
+}
+
+.team-card:before {
+	content: "";
+	position: absolute;
+	background: inherit;
+	z-index: -1;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	box-shadow: inset 0 0 2000px var(--card-background);
+	filter: blur(10px);
+	margin: -20px;
+}
+
+.team-column{
+    width: 150px;
+    display: flex;
+    flex-direction: column;
 }
 
 .team-name {
-    margin: 3px;
-    font-size: var(--font-size-xsmall);
+    font-size: 12px;
+    text-align: right;
+    margin-top: 1rem;
 }
 
 .team-title {
-    margin: 3px;
-    font-size: var(--font-size-mini);
+    font-size: 10px;
+    text-align: right;
+    color: rgb(133, 225, 255);
 }
 
 .team-info {
     display: flex;
     flex-direction: row;
-    justify-content: center;
+    justify-content: end;
 }
 
 .button-team {
     padding: 8px;
-    margin: 5px;
+    margin-top: 3rem;
+}
+#top-one{
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+}
+#last-one{
+    margin-top: 1rem;
+    margin-bottom: 2rem;
 }
 </style>
