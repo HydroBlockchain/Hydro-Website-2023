a4d1a82d89de537ba6cef8278c579c839c848362
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-10 21:30:18 +0100

    animated Hydro & wex logos

--- src/lib/components/tech/keresverse.svelte
+++ src/lib/components/tech/keresverse.svelte
@@ -12,7 +12,26 @@ import item from "../../json/techlinks.json"
         <img src={vox} alt="keres-agent" id="vox"/>
 
         <div class="middle">
-            <img src={wex} alt="keres-logo" id="wex"/>
+            <svg width="130" height="130" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M258.184 466.602L275.66 457.864L332.456 429.466L371.777 407.621L400.175 393.422L405.636 390.692L411.097 387.961L454.786 363.932L439.495 342.087L437.447 338.811L435.399 335.534L406.728 289.66L399.447 278.738L395.806 273.277L358.67 217.573L306.243 136.748L258.184 66.8447L212.311 136.748L159.883 217.573L122.748 273.277L111.825 289.66L94.3495 315.874L81.2427 335.534L79.7864 337.718L76.8738 342.087L72.5049 349.369L63.767 363.932L107.456 387.961L117.772 393.422L124.932 397.213L144.592 407.621L186.097 429.466L242.893 457.864L258.184 466.602ZM114.01 348.641L139.961 309.32L199.073 219.757L228.629 174.976L258.184 130.194L288.188 174.976L316.728 217.573L378.199 309.32L404.544 348.641L295.883 405.404L284.795 411.196L258.184 425.097L231.971 411.196L221.049 405.404L114.01 348.641Z" fill="var(--text-color)" class="svg-elem-1"></path>
+                <path d="M258.184 45L63.767 217.573L70.3204 223.762L73.0188 226.311L121.071 271.694L139.272 241.602L134.227 236.803L130.024 232.806L114.01 217.573L134.227 200.097L142.408 193.026L197.407 145.485L258.184 45Z" fill="var(--text-color)" class="svg-elem-2"></path>
+                <path d="M452.602 217.573L258.184 45L319.772 145.485L371.777 190.878L382.338 200.097L402.359 217.573L387.35 232.806L383.411 236.803L378.682 241.602L397.236 271.874L443.693 226.311L446.049 224L452.602 217.573Z" fill="var(--text-color)" class="svg-elem-3"></path>
+                <path d="M203.573 216.265L258.184 173.883L314.363 216.265L283.612 170.478L258.184 145.485L233.137 170.478L203.573 216.265Z" fill="var(--text-color)" class="svg-elem-4"></path>
+                <path d="M63.767 326.796L107.456 294.204L112.134 286.471L57.2136 326.796L79.5622 337.61L81.2427 335.534L63.767 326.796Z" fill="var(--text-color)" class="svg-elem-5"></path>
+                <path d="M231.971 411.196L258.184 416.359L284.795 411.196L295.883 405.404L258.184 411.99L221.049 405.404L231.971 411.196Z" fill="var(--text-color)" class="svg-elem-6"></path>
+                <path d="M373.291 232.806L383.411 236.803L387.35 232.806L369.427 226.501L373.291 232.806Z" fill="var(--text-color)" class="svg-elem-7"></path>
+                <path d="M371.777 190.878L337.847 174.976L340.722 179.666L382.338 200.097L371.777 190.878Z" fill="var(--text-color)" class="svg-elem-8"></path>
+                <path d="M179.57 174.976L142.408 193.026L134.227 200.097L176.733 179.666L179.57 174.976Z" fill="var(--text-color)" class="svg-elem-9"></path>
+                <path d="M134.227 236.803L144.592 232.806L148.406 226.501L130.024 232.806L134.227 236.803Z" fill="var(--text-color)" class="svg-elem-10"></path>
+                <path d="M400.175 393.422L481 381.408L438.253 338.405L437.447 338.811L439.495 342.087L470.078 378.786L405.636 390.692L400.175 393.422Z" fill="var(--text-color)" class="svg-elem-11"></path>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M317.618 220.34L258.184 178.252L201.346 219.757L148.406 302.767L258.184 216.265L372.484 304.951L317.618 220.34Z" fill="var(--text-color)" class="svg-elem-12"></path>
+                <path d="M443.693 226.311L452.602 230.68L442.937 239.417L400.731 277.576L406.183 286.471L452.602 250.34L445.53 242.347L459.155 230.68L446.049 224L443.693 226.311Z" fill="var(--text-color)" class="svg-elem-13"></path>
+                <path d="M63.767 230.68L73.0188 226.311L70.3204 223.762L57.2136 230.68L71.4824 241.602L63.767 250.34L112.134 286.471L116.811 278.738L73.4113 239.417L63.767 230.68Z" fill="var(--text-color)" class="svg-elem-14"></path>
+                <path d="M117.772 393.422L44.1068 381.408L72.5049 349.369L76.8738 342.087L79.7864 337.718L78.6518 338.734L31 381.408L124.932 397.213L117.772 393.422Z" fill="var(--text-color)" class="svg-elem-15"></path>
+                <path d="M410.922 294.204L454.786 326.796L435.399 335.534L437.447 338.811L438.253 338.405L461.34 326.796L406.183 286.471L410.922 294.204Z" fill="var(--text-color)" class="svg-elem-16"></path>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M148.406 331.165L258.184 241.602L369.592 331.165L258.184 381.408L148.406 331.165ZM258.184 372.67L358.67 331.165L323.88 303.938L258.184 363.932L193.917 303.938L159.883 331.165L258.184 372.67ZM258.184 252.524L199.204 299.709L258.184 357.379L318.476 299.709L258.184 252.524Z" fill="var(--text-color)" class="svg-elem-17"></path>
+                </svg>
+                
             <img src={keres} alt="keres-logo" id="keresverse-logo"/>
             Manifest your skills to earn!
             <a href="{item[0].link}" target="_blank" rel="noopener noreferrer" >
@@ -43,7 +62,7 @@ import item from "../../json/techlinks.json"
     margin-left: -2rem;
 }
 
-#wex {
+svg {
     height: 130px;
 }
 
@@ -78,4 +97,436 @@ import item from "../../json/techlinks.json"
     margin-top: 3rem;
     width: 100px;
 }
+
+/***************************************************
+ * Generated by SVG Artista on 12/10/2022, 9:28:09 PM
+ * MIT license (https://opensource.org/licenses/MIT)
+ * W. https://svgartista.net
+ **************************************************/
+
+ @-webkit-keyframes animate-svg-fill-1 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-1 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-1 {
+  -webkit-animation: animate-svg-fill-1 0.7s ease-in 0.8s both;
+          animation: animate-svg-fill-1 0.7s ease-in 0.8s both;
+}
+
+@-webkit-keyframes animate-svg-fill-2 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-2 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-2 {
+  -webkit-animation: animate-svg-fill-2 0.7s ease-in 0.9s both;
+          animation: animate-svg-fill-2 0.7s ease-in 0.9s both;
+}
+
+@-webkit-keyframes animate-svg-fill-3 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-3 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-3 {
+  -webkit-animation: animate-svg-fill-3 0.7s ease-in 1s both;
+          animation: animate-svg-fill-3 0.7s ease-in 1s both;
+}
+
+@-webkit-keyframes animate-svg-fill-4 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-4 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-4 {
+  -webkit-animation: animate-svg-fill-4 0.7s ease-in 1.1s both;
+          animation: animate-svg-fill-4 0.7s ease-in 1.1s both;
+}
+
+@-webkit-keyframes animate-svg-fill-5 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-5 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-5 {
+  -webkit-animation: animate-svg-fill-5 0.7s ease-in 1.2000000000000002s both;
+          animation: animate-svg-fill-5 0.7s ease-in 1.2000000000000002s both;
+}
+
+@-webkit-keyframes animate-svg-fill-6 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-6 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-6 {
+  -webkit-animation: animate-svg-fill-6 0.7s ease-in 1.3s both;
+          animation: animate-svg-fill-6 0.7s ease-in 1.3s both;
+}
+
+@-webkit-keyframes animate-svg-fill-7 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-7 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-7 {
+  -webkit-animation: animate-svg-fill-7 0.7s ease-in 1.4000000000000001s both;
+          animation: animate-svg-fill-7 0.7s ease-in 1.4000000000000001s both;
+}
+
+@-webkit-keyframes animate-svg-fill-8 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-8 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-8 {
+  -webkit-animation: animate-svg-fill-8 0.7s ease-in 1.5s both;
+          animation: animate-svg-fill-8 0.7s ease-in 1.5s both;
+}
+
+@-webkit-keyframes animate-svg-fill-9 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-9 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-9 {
+  -webkit-animation: animate-svg-fill-9 0.7s ease-in 1.6s both;
+          animation: animate-svg-fill-9 0.7s ease-in 1.6s both;
+}
+
+@-webkit-keyframes animate-svg-fill-10 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-10 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-10 {
+  -webkit-animation: animate-svg-fill-10 0.7s ease-in 1.7000000000000002s both;
+          animation: animate-svg-fill-10 0.7s ease-in 1.7000000000000002s both;
+}
+
+@-webkit-keyframes animate-svg-fill-11 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-11 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-11 {
+  -webkit-animation: animate-svg-fill-11 0.7s ease-in 1.8s both;
+          animation: animate-svg-fill-11 0.7s ease-in 1.8s both;
+}
+
+@-webkit-keyframes animate-svg-fill-12 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-12 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-12 {
+  -webkit-animation: animate-svg-fill-12 0.7s ease-in 1.9000000000000001s both;
+          animation: animate-svg-fill-12 0.7s ease-in 1.9000000000000001s both;
+}
+
+@-webkit-keyframes animate-svg-fill-13 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-13 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-13 {
+  -webkit-animation: animate-svg-fill-13 0.7s ease-in 2s both;
+          animation: animate-svg-fill-13 0.7s ease-in 2s both;
+}
+
+@-webkit-keyframes animate-svg-fill-14 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-14 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-14 {
+  -webkit-animation: animate-svg-fill-14 0.7s ease-in 2.1s both;
+          animation: animate-svg-fill-14 0.7s ease-in 2.1s both;
+}
+
+@-webkit-keyframes animate-svg-fill-15 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-15 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-15 {
+  -webkit-animation: animate-svg-fill-15 0.7s ease-in 2.2s both;
+          animation: animate-svg-fill-15 0.7s ease-in 2.2s both;
+}
+
+@-webkit-keyframes animate-svg-fill-16 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-16 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-16 {
+  -webkit-animation: animate-svg-fill-16 0.7s ease-in 2.3s both;
+          animation: animate-svg-fill-16 0.7s ease-in 2.3s both;
+}
+
+@-webkit-keyframes animate-svg-fill-17 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+@keyframes animate-svg-fill-17 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(255, 255, 255);
+  }
+}
+
+.svg-elem-17 {
+  -webkit-animation: animate-svg-fill-17 0.7s ease-in 2.4000000000000004s both;
+          animation: animate-svg-fill-17 0.7s ease-in 2.4000000000000004s both;
+}
+
 </style>
