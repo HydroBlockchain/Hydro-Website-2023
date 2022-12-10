e8b12c0b6bf9458764666235acf548359c26451c
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-10 18:51:49 +0100

    Update store.js

--- src/lib/stores/store.js
+++ src/lib/stores/store.js
@@ -11,8 +11,8 @@ const movrEndpoint = `https://api.coingecko.com/api/v3/coins/moonriver`;
 //Bscscan endpoint
 const contractAddress = `0xf3DBB49999B25c9D6641a9423C7ad84168D00071`;
 const kvsAddress = `0x587DF4d33C83e0b13cA7F45f6BD1D99F0A402646`;
-// const apiKey = import.meta.env.BSCSCAN_API_KEY;
-const bscscanEndpoint = `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=` + `${contractAddress}` + `&address=${kvsAddress}` + `&tag=latest&apikey=YKG6FZP98T89KFFPP5NS15Q5JX6QJQXJD9`;
+const apiKey = `YKG6FZP98T89KFFPP5NS15Q5JX6QJQXJD9`;
+const bscscanEndpoint = `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=` + `${contractAddress}` + `&address=${kvsAddress}` + `&tag=latest&apikey=${apiKey}`;
 
 //Medium endpoint
 const mediumEndpoint = `https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fprojecthydro.medium.com%2Ffeed`;
