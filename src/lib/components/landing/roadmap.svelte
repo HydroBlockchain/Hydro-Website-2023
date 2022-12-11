0f5a3d3f813d6a305c6500adc675f9e694efe3be
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-11 15:05:40 +0100

    basic progress bar

--- src/lib/components/landing/roadmap.svelte
+++ src/lib/components/landing/roadmap.svelte
@@ -9,23 +9,44 @@
             <div class="roadmap-card-half" id="animated-border">
                 <div class="half-card-structure">
                     <div class="card-row" id="quarter">Q1</div>
-                    <div class="card-row" id="what">What</div>
-                    <div class="card-row" id="progress">ProgressBar</div>
+                    <div class="card-row" id="what">Roadmap Entry</div>
+                    <div class="card-row" id="progress">
+                        <div class="container">    
+                            <div class="progress2 progress-moved">
+                              <div class="progress-bar2" >
+                              </div>                       
+                            </div> 
+                          </div>
+                    </div>
                 </div>
             </div>
             <div class="row">
                 <div class="roadmap-card-one" id="animated-border">
                     <div class="one-card-structure">
                         <div class="card-row-small" id="quarter">Q1</div>
-                        <div class="card-row-small" id="what-small">What</div>
-                        <div class="card-row-small" id="progress">ProgressBar</div>
+                        <div class="card-row-small" id="what-small">Roadmap Entry</div>
+                        <div class="card-row-small" id="progress">
+                            <div class="container">    
+                                <div class="progress2 progress-moved">
+                                  <div class="progress-bar2" >
+                                  </div>                       
+                                </div> 
+                              </div>
+                        </div>
                     </div>
                 </div>
                 <div class="roadmap-card-one" id="animated-border">
                     <div class="one-card-structure">
                         <div class="card-row-small" id="quarter">Q1</div>
-                        <div class="card-row-small" id="what-small">What</div>
-                        <div class="card-row-small" id="progress">ProgressBar</div>
+                        <div class="card-row-small" id="what-small">Roadmap Entry</div>
+                        <div class="card-row-small" id="progress">
+                            <div class="container">    
+                                <div class="progress2 progress-moved">
+                                  <div class="progress-bar2" >
+                                  </div>                       
+                                </div> 
+                              </div>
+                        </div>
                     </div>
                 </div>
 
@@ -33,8 +54,15 @@
             <div class="roadmap-card-half" id="animated-border">
                 <div class="half-card-structure">
                     <div class="card-row" id="quarter">Q1</div>
-                    <div class="card-row" id="what">What</div>
-                    <div class="card-row" id="progress">ProgressBar</div>
+                    <div class="card-row" id="what">Roadmap Entry</div>
+                    <div class="card-row" id="progress">
+                        <div class="container">    
+                            <div class="progress2 progress-moved">
+                              <div class="progress-bar2" >
+                              </div>                       
+                            </div> 
+                          </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -44,31 +72,59 @@
                 <div class="roadmap-card-one" id="animated-border">
                     <div class="one-card-structure">
                         <div class="card-row-small" id="quarter">Q1</div>
-                        <div class="card-row-small" id="what-small">What</div>
-                        <div class="card-row-small" id="progress">ProgressBar</div>
+                        <div class="card-row-small" id="what-small">Roadmap Entry</div>
+                        <div class="card-row-small" id="progress">
+                            <div class="container">    
+                                <div class="progress2 progress-moved">
+                                  <div class="progress-bar2" >
+                                  </div>                       
+                                </div> 
+                              </div>
+                        </div>
                     </div>
                 </div>
                 <div class="roadmap-card-one" id="animated-border">
                     <div class="one-card-structure">
                         <div class="card-row-small" id="quarter">Q1</div>
-                        <div class="card-row-small" id="what-small">What</div>
-                        <div class="card-row-small" id="progress">ProgressBar</div>
+                        <div class="card-row-small" id="what-small">Roadmap Entry</div>
+                        <div class="card-row-small" id="progress">
+                            <div class="container">    
+                                <div class="progress2 progress-moved">
+                                  <div class="progress-bar2" >
+                                  </div>                       
+                                </div> 
+                              </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="roadmap-card-half" id="animated-border">
                 <div class="half-card-structure">
                     <div class="card-row" id="quarter">Q1</div>
-                    <div class="card-row" id="what">What</div>
-                    <div class="card-row" id="progress">ProgressBar</div>
+                    <div class="card-row" id="what">Roadmap Entry</div>
+                    <div class="card-row" id="progress">
+                        <div class="container">    
+                            <div class="progress2 progress-moved">
+                              <div class="progress-bar2" >
+                              </div>                       
+                            </div> 
+                          </div>
+                    </div>
                 </div>
             </div>
 
             <div class="roadmap-card-half" id="animated-border">
                 <div class="half-card-structure">
                     <div class="card-row" id="quarter">Q1</div>
-                    <div class="card-row" id="what">What</div>
-                    <div class="card-row" id="progress">ProgressBar</div>
+                    <div class="card-row" id="what">Roadmap Entry</div>
+                    <div class="card-row" id="progress">
+                        <div class="container">    
+                            <div class="progress2 progress-moved">
+                              <div class="progress-bar2" >
+                              </div>                       
+                            </div> 
+                          </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -162,4 +218,34 @@
     width: 500px;
     height: 150px;
 }
+
+.container {
+  width: 500px;
+  text-align: center;
+  margin-top: 1rem;
+}
+ 
+.progress2 {
+  padding: 3px;
+  border-radius: 7px;
+}
+
+.progress-bar2 {
+  height: 10px;
+  border-radius: 7px;
+  transition: 0.4s linear;  
+  transition-property: width, background-color;    
+}
+
+.progress-moved .progress-bar2 {
+  width: 85%; 
+  background-color:rgba(83, 246, 255, 0.8);  
+  animation: progressAnimation 6s;
+}
+
+@keyframes progressAnimation {
+  0%   { width: 5%; background-color: rgba(83, 246, 255, 0.5);}
+  100% { width: 85%; background-color: rgba(83, 246, 255, 0.75); }
+}
+
 </style>
