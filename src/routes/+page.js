c99780df70d75855f75080a899a158bdbfc0f7ca
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-14 12:33:19 +0100

    another test

--- src/routes/+page.js
+++
@@ -1,6 +0,0 @@
-import { dev, browser } from '$app/environment';
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
-import { fetchData } from "$lib/stores/fetch";
-fetchData()
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +8,8 @@ const config = {
     kit: {
         adapter: adapter({
             fallback: 'index.html',
+            edge: false,
+            split: false
         })
     },
     preprocess: [
