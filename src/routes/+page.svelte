eec50317f71055553f7e41ea5dd244b1b88064e2
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-16 12:58:46 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,30 @@
 <script>
-//Components
+// //Components
+
+import Medium from "$lib/components/landing/Medium.svelte";
+import Pricewidget from "$lib/components/landing/Pricewidget.svelte";
+import Roadmap from "$lib/components/landing/Roadmap.svelte";
 import Hero from "$lib/components/landing/Hero.svelte";
 import Partners from "$lib/components/landing/Partners.svelte";
 import Exchanges from "$lib/components/landing/Exchanges.svelte";
 import Featured from "$lib/components/landing/Featured.svelte";
-import Medium from "$lib/components/landing/Medium.svelte";
-import Pricewidget from "$lib/components/landing/Pricewidget.svelte";
-import Roadmap from "$lib/components/landing/Roadmap.svelte";
 </script>
 <svelte:head>
     <title>Home</title>
     <meta name="Project Hydro" content="Project Hydro Website" />
 </svelte:head>
     <section>
-        <Hero />
+        <Hero/>
         <div class="multiple-containers">
             <Pricewidget/>
             <Medium/>
         </div>
         <Roadmap/>
         <div class="multiple-containers-alt">
-            <Partners />
-            <Exchanges />
+            <Partners/>
+            <Exchanges/>
         </div>
-        <Featured />
+        <Featured/>
     </section>
 <style>
 </style>
