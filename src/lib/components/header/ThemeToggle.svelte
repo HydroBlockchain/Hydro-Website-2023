4371bdfdb211f42554ce168f998751ba4ef87359
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-12-18 12:12:05 +0100

    save theme in local storage + style updates

--- src/lib/components/header/ThemeToggle.svelte
+++ src/lib/components/header/ThemeToggle.svelte
@@ -1,22 +1,61 @@
-<script>
-    import Sun from "$lib/images/icons/sun.svg"
-    import Moon from "$lib/images/icons/moon.svg"
+<script lang="ts">
+// @ts-nocheck
+import Sun from "$lib/images/icons/sun.svg"
+import Moon from "$lib/images/icons/moon.svg"
+import { onMount } from 'svelte';
 
-	let darkMode = false;
-	function toggle() {
-        darkMode = !darkMode;
-        window.document.body.classList.toggle('dark');
+  const STORAGE_KEY = 'theme';
+  const DARK_PREFERENCE = '(prefers-color-scheme: dark)';
+
+  const THEMES = {
+    DARK: 'dark',
+    LIGHT: 'light',
+  };
+
+  const prefersDarkThemes = () => window.matchMedia(DARK_PREFERENCE).matches;
+
+  const toggleTheme = () => {
+    const stored = localStorage.getItem(STORAGE_KEY);
+
+    if (stored) {
+      // clear storage
+      localStorage.removeItem(STORAGE_KEY);
+    } else {
+      // store opposite of preference
+      localStorage.setItem(STORAGE_KEY, prefersDarkThemes() ? THEMES.LIGHT : THEMES.DARK);
     }
-</script>
 
+    applyTheme();
+  };
+
+  let currentTheme = '';
+
+  const applyTheme = () => {
+    const preferredTheme = prefersDarkThemes() ? THEMES.DARK : THEMES.LIGHT;
+    currentTheme = localStorage.getItem(STORAGE_KEY) ?? preferredTheme;
+
+    if (currentTheme === THEMES.DARK) {
+      document.body.classList.remove(THEMES.LIGHT);
+      document.body.classList.add(THEMES.DARK);
+    } else {
+      document.body.classList.remove(THEMES.DARK);
+      document.body.classList.add(THEMES.LIGHT);
+    }
+  };
+
+  onMount(() => {
+    applyTheme();
+    window.matchMedia(DARK_PREFERENCE).addEventListener('change', applyTheme);
+  });
+</script>
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class="themeDiv" on:click={toggle}>
-	{#if darkMode }
+<div class="themeDiv" checked={currentTheme !== THEMES.DARK} on:click={toggleTheme}>
+	{#if THEMES.DARK}
     <img src={Moon} alt="bitcoin" id="themeToggle" />
 	{:else}
     <img src={Sun} alt="bitcoin" id="themeToggle" />
 	{/if}
-</div>
+</div> 
 
 <style>
 .themeDiv{
@@ -27,5 +66,4 @@
     width: 32px;
     height: 32px;
 }
-
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -26,7 +26,7 @@ function handleClick() {
 						<g><path fill="none" d="M0 0h24v24H0z"/><path d="M16 18v2H5v-2h11zm5-7v2H3v-2h18zm-2-7v2H8V4h11z" fill="var(--text-color)"/></g>
 					</svg>
                     <div class="navigation">
-                        <ThemeToggle/>
+                          <ThemeToggle/>
                         <a data-sveltekit-prefetch href="/tech">
                             <img src={techIcon} alt="Technology" id="icon" /></a>
                         <a data-sveltekit-prefetch href="/team">
